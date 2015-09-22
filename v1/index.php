a20ea549208556bde0a7cd4787dbf92c227fbf95
Zack Gilbert <zackgilbert@gmail.com>
2015-09-21 19:43:03 -0500

    Add versions of site. Including new version.

---
+++ v1/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Zack Gilbert - Pure Awesomeness</title>
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=utf-8" />
+<meta http-equiv="X-XRDS-Location" content="http://zackgilbert.myopenid.com/xrds" />
+<meta name="description" content="My name is Zack Gilbert. I currently live in Chicago, IL and am the developer half of The AGC Inc., the parent company of Seen Creative" />
+<meta name="keywords" content="zack gilbert, zachary gilbert, zach gilbert, seen creative, seencreative, weareseencreative, billq, mybillq, agc, chicago, rochester, new york, web development, internet" />
+<meta name="verify-v1" content="EuT2AKn5qXOeaUbRB9RSEda0xmhEKu66qdPplTNHIds=" />
+<link rel="openid.server" href="http://www.myopenid.com/server" />
+<link rel="openid.delegate" href="http://zackgilbert.myopenid.com/" />
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="http://feeds.feedburner.com/zackgilbert" />
+
+<style type="text/css">
+
+	body {
+		font-family: Helvetica, Arial, sans-serif;
+		background-color: #252835;
+		color: #EEEED4;
+	}
+		a {
+			color: #5BA6CF;
+			text-decoration: none;
+		}
+	
+		#container {
+			width: 550px;
+			margin: 150px auto;
+		}
+		
+			#container img {
+				float: left;
+				margin: 5px 20px 40px 0px;
+				width: 120px;
+				height: 120px;
+				border: 5px solid #5ba6cf;
+			}
+		
+			#header {}
+				#header h1 {
+					margin: 0px 0px 16px 0px;
+					font-size: 32px;
+					font-family: Georgia, Times, serif;
+				}
+		
+			#content {}
+			
+				#content p {
+					margin: 0px 0px 16px 0px;
+					font-size: 14px;
+					color: #eeeed4;
+					line-height: 20px;
+				}
+				#content h3 {
+					margin-bottom: 0px;
+					font-weight: 400;
+					color: #40424e;
+				}
+		
+</style>
+
+</head>
+<body>
+	
+	<div id="container" class="vcard">
+	
+		<img src="me.jpg" alt="Me"/>
+	
+		<div id="header">
+			<h1>Hi, I'm <span class="fn">Zack Gilbert</span></h1>
+		</div>
+		
+		<div id="content">
+		
+			<p>
+				
+				I am co-founder and co-owner of <a href="http://theagcinc.com/">The AGC Inc.</a>, the parent company of <a href="http://www.areyouseen.com">Seen Creative</a> and creators of <a href="http://www.mybillq.com">billQ</a> and <a href="http://www.flintpublisher.com">Flint</a>. I'm originally from western/upstate New York, but currently live in <span class="locality">Chicago, IL</span>.
+				
+			</p>
+			
+			<p>
+				
+				Find more of me at <a href="http://www.facebook.com/zackgilbert">Facebook</a>, <a href="http://zackgilbert.tumblr.com" rel="me">Tumblr</a></li>, <a href="http://www.twitter.com/zackgilbert" rel="me">Twitter</a>, and <a href="http://www.last.fm/user/zackgilbert" rel="me">Last.fm</a>.
+				
+			</p>
+		
+		</div>
+			
+	</div>
+
+</body>
+</html>
